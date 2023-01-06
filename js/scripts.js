965a59548eb7d8a737991a5454f1733480561f48
Manickam Somasundaram <manickam.som@gmail.com>
2023-01-06 17:55:13 +0100

    feat: find big pokemons with a for loop
    for loop loops around all pokemons in list and using conditional statements sorts it by height. css was also changed to display everything in Ubuntu font. the css file was renamed styles from style

--- js/scripts.js
+++ js/scripts.js
@@ -12,8 +12,39 @@ let pokemonList = [
     },
 
     {
+        name: "Bulbasaur",
+        height: 0.7,
+        type: ["grass", "poison"]
+    },
+
+    {
+        name: "Ivysaur",
+        height: 1,
+        type: ["grass", "poison"]
+    },
+
+    {
+        name: "Venusaur",
+        height: 2,
+        type: ["grass", "poison"]
+    },
+
+    {
         name: "Metapod",
         height : 0.7,
         type: "bug",
     }
 ]
+
+for (let i = 0; i < pokemonList.length; i++) {
+    // for the pokemons with height more than 1m, Wow that's big shall be shown next to it
+    if (pokemonList[i].height >=1) {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m) - Wow, that's big!" + "<br>")
+        }
+    else if (pokemonList[i].height >= 0.6 && pokemonList[i].height < 1) {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m) - That's a small pokemon!" + "<br>")
+        }
+    else {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m) - That's a very small pokemon!" + "<br>")
+        }
+}
